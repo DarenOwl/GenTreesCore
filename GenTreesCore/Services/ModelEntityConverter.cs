ae09830bccb12f4e6cf12ec73114c7f192c46190
DarenOwl <DarenOwl@users.noreply.github.com>
2020-08-12 22:44:40 +0500

    Убрала обновление дерева из конвертера моделей

--- GenTreesCore/Services/ModelEntityConverter.cs
+++ GenTreesCore/Services/ModelEntityConverter.cs
@@ -1,22 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using GenTreesCore.Entities;
 using GenTreesCore.Models;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Migrations.Operations;
-using System.Data.Common;
 
 namespace GenTreesCore.Services
 {
     public class ModelEntityConverter
     {
-        ApplicationContext db;
 
-        public ModelEntityConverter(ApplicationContext context)
-        {
-            db = context;
-        }
         #region ToViewModel
         public GenTreeViewModel ToViewModel(GenTree tree)
         {
@@ -80,8 +71,7 @@ namespace GenTreesCore.Services
                 Id = relation.Id,
                 TargetPersonId = relation.TargetPerson.Id,
                 SecondParentId = null,
-                RelationRate = relation.RelationRate.ToString(),
-                RelationType = "ChildRelation"
+                RelationRate = relation.RelationRate.ToString()
             };
             if (relation.SecondParent != null)
                 childRelationModel.SecondParentId = relation.SecondParent.Id;
@@ -94,8 +84,7 @@ namespace GenTreesCore.Services
             {
                 Id = relation.Id,
                 TargetPersonId = relation.TargetPerson.Id,
-                IsFinished = relation.IsFinished,
-                RelationType = "SpouseRelation"
+                IsFinished = relation.IsFinished
             };
         }
 
@@ -118,68 +107,46 @@ namespace GenTreesCore.Services
 
         #endregion
 
-        #region ToEntityModel
-        public Person ToEntityModel(PersonViewModel model)
+        #region ToEntity
+        public Person ToEntity(PersonViewModel model, GenTree tree)
         {
-            return new Person
-            {
-                LastName = model.LastName,
-                FirstName = model.FirstName,
-                MiddleName = model.MiddleName,
-                BirthPlace = model.BirthPlace,
-                Biography = model.Biography,
-                Gender = model.Gender,
-                Image = model.Image,
-                CustomDescriptions = model.CustomDescriptions
-            };
+            var entity = new Person();
+            ApplyModelData(entity, model, tree);
+            return entity;
         }
 
-        public Relation ToEntityModel(RelationViewModel model, GenTree tree)
+        public Relation ToEntity(RelationViewModel model, GenTree tree)
         {
-            if (model is SpouseRelationViewModel)
+
+            if (model.GetType() == typeof(SpouseRelationViewModel))
                 return new SpouseRelation
                 {
                     TargetPerson = tree.Persons.FirstOrDefault(p => p.Id == model.TargetPersonId),
                     IsFinished = (model as SpouseRelationViewModel).IsFinished
                 };
-            else if (model is ChildRelationViewModel)
+            else if (model.GetType() == typeof(ChildRelationViewModel))
+            {
                 return new ChildRelation
                 {
                     TargetPerson = tree.Persons.FirstOrDefault(p => p.Id == model.TargetPersonId),
                     RelationRate = (RelationRate)Enum.Parse(typeof(RelationRate), (model as ChildRelationViewModel).RelationRate),
                     SecondParent = tree.Persons.FirstOrDefault(p => p.Id == (model as ChildRelationViewModel).SecondParentId)
                 };
+            }
             else return null;
         }
         #endregion
 
-        public void UpdateEntity(GenTreeViewModel model, GenTree tree)
+        #region ApplyModelData
+        public void ApplyModelData(GenTree entity, GenTreeViewModel model)
         {
-            //обновление данных дерева
-            tree.Name = model.Name;
-            tree.Description = model.Description;
-            tree.IsPrivate = model.IsPrivate;
-            tree.LastUpdated = DateTime.Now;
-            tree.Image = model.Image;
-
-            //обновление шаблонов описаний
-            ApplyChanges(tree.CustomPersonDescriptionTemplates, model.DescriptionTemplates, e => e.Id, m => m.Id,
-                m => tree.CustomPersonDescriptionTemplates.Add(m), (e, m) => ApplyModelData(e, m),
-                e => db.Set<CustomPersonDescriptionTemplate>().Remove(e));
-
-            //обновление людей-узлов дерева
-            ApplyChanges(tree.Persons, model.Persons, e => e.Id, m => m.Id,
-                m =>
-                {
-                    var newPerson = new Person();
-                    tree.Persons.Add(newPerson);
-                    ApplyModelData(newPerson, m, tree);
-                },
-                (e, m) => ApplyModelData(e, m, tree),
-                e => db.Set<Person>().Remove(e));
+            entity.Name = model.Name;
+            entity.Description = model.Description;
+            entity.IsPrivate = model.IsPrivate;
+            entity.LastUpdated = DateTime.Now;
+            entity.Image = model.Image;
         }
 
-        #region ApplyModelData
         public void ApplyModelData(Person entity, PersonViewModel model, GenTree tree)
         {
             entity.LastName = model.LastName;
@@ -197,8 +164,8 @@ namespace GenTreesCore.Services
                 if (entity.BirthDate == null)
                     entity.BirthDate = new GenTreeDateTime();
                 ApplyModelData(entity.BirthDate, model.BirthDate, tree);
-            }    
-                
+            }
+
             //обновляем дату смерти
             if (model.DeathDate == null)
                 entity.DeathDate = null;
@@ -208,26 +175,6 @@ namespace GenTreesCore.Services
                     entity.DeathDate = new GenTreeDateTime();
                 ApplyModelData(entity.DeathDate, model.DeathDate, tree);
             }
-
-            //обновляем пользовательские описания
-            ApplyChanges(entity.CustomDescriptions, model.CustomDescriptions, e => e.Id, m => m.Id,
-                m =>
-                {
-                    var template = tree.CustomPersonDescriptionTemplates.FirstOrDefault(t => t.Id == m.Template.Id);
-                    if (template != null)
-                    {
-                        m.Template = template;
-                        entity.CustomDescriptions.Add(m);
-                    }
-                },
-                (e, m) => ApplyModelData(e, m), 
-                e => db.Set<CustomPersonDescription>().Remove(e));
-
-            //обновляем отношения
-            ApplyChanges(entity.Relations, model.Relations, e => e.Id, m => m.Id,
-                m => entity.Relations.Add(ToEntityModel(m, tree)),
-                (e, m) => ApplyModelData(e, m),
-                e => db.Set<Relation>().Remove(e));
         }
 
         public void ApplyModelData(CustomPersonDescription entity, CustomPersonDescription model)
@@ -263,59 +210,21 @@ namespace GenTreesCore.Services
             entity.Second = model.Second;
         }
 
-        #endregion 
-
-        /// <summary>
-        /// Для пар модель данных - мдель представления выполняет действие добавления для пары без модели данных и действие обновления для пары view-entity
-        /// </summary>
-        /// <typeparam name="E">Модель данных</typeparam>
-        /// <typeparam name="M">Модель представления</typeparam>
-        /// <typeparam name="TKey">Ключ для сравнения</typeparam>
-        public void ApplyChanges<E,M,TKey>(IEnumerable<E> entities, IEnumerable<M> models,
-            Func<E,TKey> entityKeySelector, Func<M,TKey> modelKeySelector,
-            Action<M> add, Action<E,M> update, Action<E> delete)
+        public void ApplyModelData(GenTreeDateTimeSetting entity, GenTreeDateTimeSetting model)
         {
-            if (entities == null && models != null)
-            {
-                foreach (var model in models)
-                    add(model);
-                return;
-            }
-
-            //формируем коллекцию пар entity-viewmodel
-            var collection = from model in models
-                             join entity in entities
-                             on modelKeySelector(model) equals entityKeySelector(entity)
-                             into temp
-                             from entityOrNull in temp.DefaultIfEmpty()
-                             select new ModelEntityPair<E, M>(entityOrNull, model);
-
-            //применияем действия обновления и добавления
-            collection.ToList().ForEach(pair =>
-            {
-                if (pair.Entity == null)
-                    add(pair.Model);
-                else
-                    update(pair.Entity, pair.Model);
-            });
-
-            //удаляем элементы, которые были удалены из модели
-            var forDelete = entities
-                .Where(e => models.FirstOrDefault(m => modelKeySelector(m).Equals(entityKeySelector(e))) == null).ToList();
-            foreach (var element in forDelete)
-                delete(element);
+            entity.Name = model.Name;
+            entity.IsPrivate = model.IsPrivate;
+            entity.YearMonthCount = model.YearMonthCount;
         }
-    }
 
-    public class ModelEntityPair<TEntity, TModel>
-    {
-        public TEntity Entity { get; }
-        public TModel Model { get; }
-
-        public ModelEntityPair(TEntity entity, TModel model)
+        public void ApplyModelData(GenTreeEra entity, GenTreeEra model)
         {
-            Entity = entity;
-            Model = model;
+            entity.Name = model.Name;
+            entity.ShortName = model.ShortName;
+            entity.Description = model.Description;
+            entity.ThroughBeginYear = model.ThroughBeginYear;
+            entity.YearCount = model.YearCount;
         }
+        #endregion
     }
 }
